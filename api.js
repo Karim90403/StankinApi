ad6c3d9bd1715aea2ff6cbc45cdb583f66ac470f
Karim90403 <karim90403@gmail.com>
2023-02-08 01:48:57 +0300

    Составлен запрос на получение токена

--- api.js
+++ api.js
@@ -1,22 +1,17 @@
 require('dotenv').config()
 const express = require('express');
 const app = express.Router();
+const axios = require('axios');
+const fs = require('fs');
 
 app.post("/getToken", async (req, res) => {
     try {
-        const params = new URLSearchParams();
-        params.append('code', `${req.body.code}`);
-        params.append('client_id', `${process.env.client_id}`);
-        params.append('client_secret', `${process.env.client_secret}`);
-        console.log(params);
-        let reqest = await axios.post("https://lk.stankin.ru/webapi/oauth/token", {
-            data: params,
-            headers: {
-                'Content-type': 'application/x-www-form-urlencoded',
-            }
-        })
-
-        res.status(200).json({ reqest })
+        const reqParams = new URLSearchParams();
+        reqParams.append('code', `${req.body.code}`);
+        reqParams.append('client_id', `${process.env.client_id}`);
+        reqParams.append('client_secret', `${process.env.client_secret}`);
+        let reqest = await axios.post("https://lk.stankin.ru/webapi/oauth/token", reqParams, "application/x-www-form-urlencoded")
+        res.status(200).json(reqest.data)
     } catch (err) {
         res.status(400).json({ error: err.message });
         console.log(err);
--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const api = require('./api');
 const path = require('path');
-const https = require("https");
+const helmet = require("helmet");
+
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
 
 app.use("/api", api)
 app.use(express.static(path.join(__dirname, "public")))
 
-
-https
-    .createServer(app)
-    .listen(process.env.PORT, () => {        
-        console.log(`App ran at ${process.env.PORT}`)
-    })
+app.listen(process.env.PORT, () => {
+    console.log(`App ran at ${process.env.PORT}`)
+})
